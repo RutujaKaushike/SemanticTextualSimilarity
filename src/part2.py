095fd671d09ae3d38a7e7cdb15e036c33c17d03b
RutujaKaushike <rutujakaushike@gmail.com>
2019-12-05 16:32:50 -0600

    Completed Part2

--- src/part2.py
+++ src/part2.py
@@ -5,6 +5,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk import pos_tag
+from nltk.corpus import wordnet
 
 from spacy import displacy
 
@@ -34,6 +35,48 @@ def display_dependency_parse_tree(text):
     text_to_be_shown = spacy_model(text)
     displacy.serve(text_to_be_shown, style="dep")
 
+def hypernym_text(word):
+    lemmatizer = WordNetLemmatizer()
+    lemma = lemmatizer.lemmatize(word)
+    if(lemma):
+        most_frequent_synset_of_lemma = wordnet.synsets(lemma)[0]
+        print(most_frequent_synset_of_lemma.hypernyms())
+    else:
+        print("Please enter a valid word")
+
+def hyponym_text(word):
+    lemmatizer = WordNetLemmatizer()
+    lemma = lemmatizer.lemmatize(word)
+    if(lemma):
+        most_frequent_synset_of_lemma = wordnet.synsets(lemma)[0]
+        print(most_frequent_synset_of_lemma.hyponyms())
+    else:
+        print("Please enter a valid word")
+
+def meronym_text(word):
+    lemmatizer = WordNetLemmatizer()
+    lemma = lemmatizer.lemmatize(word)
+    if(lemma):
+        most_frequent_synset_of_lemma = wordnet.synsets(lemma)[0]
+        print("part meronyms are\n")
+        print(most_frequent_synset_of_lemma.part_meronyms())  # for part meronyms
+        print("\n substance meronyms are\n")
+        print(most_frequent_synset_of_lemma.substance_meronyms()) # for substance meronyms
+    else:
+        print("Please enter a valid word")
+
+def holonym_text(word):
+    lemmatizer = WordNetLemmatizer()
+    lemma = lemmatizer.lemmatize(word)
+    if(lemma):
+        most_frequent_synset_of_lemma = wordnet.synsets(lemma)[0]
+        print("part holonyms are\n")
+        print(most_frequent_synset_of_lemma.part_holonyms())  # for part holonyms
+        print("\n substance holonyms are\n")
+        print(most_frequent_synset_of_lemma.substance_holonyms()) # for substance holonyms
+    else:
+        print("Please enter a valid word")
+
 if __name__ == '__main__':
     print("\nPlease select operation - \n"\
           "1. Tokenize data \n"\
@@ -41,28 +84,55 @@ if __name__ == '__main__':
           "3. Remove stop words from data\n"\
           "4. Display part-of-speech (pos) tagging\n"\
           "5. Display dependency parse tree\n"\
-          "6. Quit\n")
+          "6. Hypernym \n"\
+          "7. Hyponym \n"\
+          "8. Meronym \n"\
+          "9. Holonym\n"\
+          "10. Quit\n")
 
-    select = input("Select operations form 1, 2, 3, 4, 5 or 6:\n")
+    select = input("Select operations form 1, 2, 3, 4, 5, 6, 7, 8, 9 or 10:\n")
 
-    while select != '6':
-        text = input("Enter text:\n")
-
+    while select != '10':
         if select == '1':
+            text = input("Enter text:\n")
             print("Tokenized data is: \n")
             print(tokenize_text(text))
         elif select == '2':
+            text = input("Enter text:\n")
             print("Lemmatized data is: \n")
             print(lemmatize_text(text))
         elif select == '3':
+            text = input("Enter text:\n")
             print("Cleaned data after removing stop words is: \n")
             print(stop_word_remove_text(text))
         elif select == '4':
+            text = input("Enter text:\n")
             print("POS tagged data is: \n")
             print(partofspeech_tag(text))
         elif select == '5':
+            text = input("Enter text:\n")
             print("Displaying the dependency parse tree \n")
             display_dependency_parse_tree(text)
+        elif select == '6':
+            text = input("Enter a word:\n")
+            print("Hypernyms are: \n")
+            lemmas = lemmatize_text(text)
+            hypernym_text(lemmas[0])
+        elif select == '7':
+            text = input("Enter a word:\n")
+            print("Hyponyms are: \n")
+            lemmas = lemmatize_text(text)
+            hyponym_text(lemmas[0])
+        elif select == '8':
+            text = input("Enter a word:\n")
+            print("Meronyms are: \n")
+            lemmas = lemmatize_text(text)
+            meronym_text(lemmas[0])
+        elif select == '9':
+            text = input("Enter a word:\n")
+            print("Holonyms are: \n")
+            lemmas = lemmatize_text(text)
+            holonym_text(lemmas[0])
         else:
             print("Please select valid operation.")
 
@@ -72,7 +142,12 @@ if __name__ == '__main__':
               "3. Remove stop words from data\n" \
               "4. Display part-of-speech (pos) tagging\n" \
               "5. Display dependency parse tree\n" \
-              "6. Quit\n")
-        select = input("Select operations form 1, 2, 3, 4, 5 or 6:\n")
+              "6. Hypernym \n"
+              "7. Hyponym \n" \
+              "8. Meronym \n" \
+              "9. Holonym\n" \
+              "10. Quit\n")
+
+        select = input("Select operations form 1, 2, 3, 4, 5, 6, 7, 8, 9 or 10:\n")
 
     print("Proceeding to exit")
