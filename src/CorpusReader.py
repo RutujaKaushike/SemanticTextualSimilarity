03a21c8b4edf437e62c8f8c5d5a6f760166158c6
RutujaKaushike <rutujakaushike@gmail.com>
2019-12-06 09:13:56 -0600

    Created emsemble model

--- src/CorpusReader.py
+++ src/CorpusReader.py
@@ -2,14 +2,19 @@ import difflib
 import pprint
 import argparse
 import string
+import re
+
+from gensim.parsing.preprocessing import stem
 
 import sklearn
 from collections import defaultdict
 from nltk.corpus import wordnet_ic
+from nltk.metrics import edit_distance
+from nltk.corpus import stopwords
 
 from googletrans import Translator
 from nltk import pos_tag
-from sklearn.ensemble import RandomForestClassifier, BaggingRegressor, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, BaggingRegressor, AdaBoostClassifier, VotingClassifier
 from sklearn.svm import SVR
 from nltk import word_tokenize, pos_tag, WordNetLemmatizer, tag
 from nltk.corpus import wordnet as wn
@@ -154,6 +159,260 @@ def bag_of_words(sen1, sen2):
     cosine = c / float((sum(vector1)*sum(vector2))**0.5)
     # print("similarity: ", cosine)
     return cosine
+
+def path(set1, set2):
+    return wn.path_similarity(set1, set2)
+
+
+def wup(set1, set2):
+    return wn.wup_similarity(set1, set2)
+
+
+def edit(word1, word2):
+    if float(edit_distance(word1, word2)) == 0.0:
+        return 0.0
+    return 1.0 / float(edit_distance(word1, word2))
+STOP_WORDS = stopwords.words()
+
+def clean_sentence(val):
+    "remove chars that are not letters or numbers, downcase, then remove stop words"
+    regex = re.compile('([^\s\w]|_)+')
+    sentence = regex.sub('', val).lower()
+    sentence = sentence.split(" ")
+
+    for word in list(sentence):
+        if word in STOP_WORDS:
+            sentence.remove(word)
+
+    sentence = " ".join(sentence)
+    return sentence
+
+def semanticSimilarity(q1, q2):
+
+    tokens_q1, tokens_q2 = tokenize(q1, q2)
+    # stem_q1, stem_q2 = stemmer(tokens_q1, tokens_q2)
+    tag_q1, tag_q2 = posTag(tokens_q1, tokens_q2)
+
+    sentence = []
+    for i, word in enumerate(tag_q1):
+        if 'NN' in word[1] or 'JJ' in word[1] or 'VB' in word[1]:
+            sentence.append(word[0])
+
+    sense1 = Lesk(sentence)
+    sentence1Means = []
+    for word in sentence:
+        sentence1Means.append(sense1.lesk(word, sentence))
+
+    sentence = []
+    for i, word in enumerate(tag_q2):
+        if 'NN' in word[1] or 'JJ' in word[1] or 'VB' in word[1]:
+            sentence.append(word[0])
+
+    sense2 = Lesk(sentence)
+    sentence2Means = []
+    for word in sentence:
+        sentence2Means.append(sense2.lesk(word, sentence))
+    # for i, word in enumerate(sentence1Means):
+    #     print sentence1Means[i][0], sentence2Means[i][0]
+
+    R1 = computePath(sentence1Means, sentence2Means)
+    R2 = computeWup(sentence1Means, sentence2Means)
+
+    R = (R1 + R2) / 2
+
+    # print R
+
+    return overallSim(sentence1Means, sentence2Means, R)
+
+def overallSim(q1, q2, R):
+
+    sum_X = 0.0
+    sum_Y = 0.0
+
+    for i in range(len(q1)):
+        max_i = 0.0
+        for j in range(len(q2)):
+            if R[i, j] > max_i:
+                max_i = R[i, j]
+        sum_X += max_i
+
+    for i in range(len(q1)):
+        max_j = 0.0
+        for j in range(len(q2)):
+            if R[i, j] > max_j:
+                max_j = R[i, j]
+        sum_Y += max_j
+
+    if (float(len(q1)) + float(len(q2))) == 0.0:
+        return 0.0
+
+    overall = (sum_X + sum_Y) / (2 * (float(len(q1)) + float(len(q2))))
+
+    return overall
+
+def computeWup(q1, q2):
+
+    R = np.zeros((len(q1), len(q2)))
+
+    for i in range(len(q1)):
+        for j in range(len(q2)):
+            if q1[i][1] == None or q2[j][1] == None:
+                sim = edit(q1[i][0], q2[j][0])
+            else:
+                sim = wup(wn.synset(q1[i][1]), wn.synset(q2[j][1]))
+
+            if sim == None:
+                sim = edit(q1[i][0], q2[j][0])
+
+            R[i, j] = sim
+
+    # print R
+
+    return R
+
+def computePath(q1, q2):
+
+    R = np.zeros((len(q1), len(q2)))
+
+    for i in range(len(q1)):
+        for j in range(len(q2)):
+            if q1[i][1] == None or q2[j][1] == None:
+                sim = edit(q1[i][0], q2[j][0])
+            else:
+                sim = path(wn.synset(q1[i][1]), wn.synset(q2[j][1]))
+
+            if sim == None:
+                sim = edit(q1[i][0], q2[j][0])
+
+            R[i, j] = sim
+
+    # print R
+
+    return R
+
+def tokenize(q1, q2):
+    """
+        q1 and q2 are sentences/questions. Function returns a list of tokens for both.
+    """
+    return word_tokenize(q1), word_tokenize(q2)
+
+
+def posTag(q1, q2):
+    """
+        q1 and q2 are lists. Function returns a list of POS tagged tokens for both.
+    """
+    return pos_tag(q1), pos_tag(q2)
+
+
+def stemmer(tag_q1, tag_q2):
+    """
+        tag_q = tagged lists. Function returns a stemmed list.
+    """
+
+    stem_q1 = []
+    stem_q2 = []
+
+    for token in tag_q1:
+        stem_q1.append(stem(token))
+
+    for token in tag_q2:
+        stem_q2.append(stem(token))
+
+    return stem_q1, stem_q2
+
+class Lesk(object):
+
+    def __init__(self, sentence):
+        self.sentence = sentence
+        self.meanings = {}
+        for word in sentence:
+            self.meanings[word] = ''
+
+    def getSenses(self, word):
+        # print word
+        return wn.synsets(word.lower())
+
+    def getGloss(self, senses):
+
+        gloss = {}
+
+        for sense in senses:
+            gloss[sense.name()] = []
+
+        for sense in senses:
+            gloss[sense.name()] += word_tokenize(sense.definition())
+
+        return gloss
+
+    def getAll(self, word):
+        senses = self.getSenses(word)
+
+        if senses == []:
+            return {word.lower(): senses}
+
+        return self.getGloss(senses)
+
+    def Score(self, set1, set2):
+        # Base
+        overlap = 0
+
+        # Step
+        for word in set1:
+            if word in set2:
+                overlap += 1
+
+        return overlap
+
+    def overlapScore(self, word1, word2):
+
+        gloss_set1 = self.getAll(word1)
+        if self.meanings[word2] == '':
+            gloss_set2 = self.getAll(word2)
+        else:
+            # print 'here'
+            gloss_set2 = self.getGloss([wn.synset(self.meanings[word2])])
+
+        # print gloss_set2
+
+        score = {}
+        for i in gloss_set1.keys():
+            score[i] = 0
+            for j in gloss_set2.keys():
+                score[i] += self.Score(gloss_set1[i], gloss_set2[j])
+
+        bestSense = None
+        max_score = 0
+        for i in gloss_set1.keys():
+            if score[i] > max_score:
+                max_score = score[i]
+                bestSense = i
+
+        return bestSense, max_score
+
+    def lesk(self, word, sentence):
+        maxOverlap = 0
+        context = sentence
+        word_sense = []
+        meaning = {}
+
+        senses = self.getSenses(word)
+
+        for sense in senses:
+            meaning[sense.name()] = 0
+
+        for word_context in context:
+            if not word == word_context:
+                score = self.overlapScore(word, word_context)
+                if score[0] == None:
+                    continue
+                meaning[score[0]] += score[1]
+
+        if senses == []:
+            return word, None, None
+
+        self.meanings[word] = max(meaning.keys(), key=lambda x: meaning[x])
+
+        return word, self.meanings[word], wn.synset(self.meanings[word]).definition()
 
 # def jaccard(s1, s2):
 #     sentence1 = pos_tag(word_tokenize(s1))
@@ -331,8 +590,9 @@ def main(args):
         scores = [
                    sentence_similarity_word_alignment(s1,s2),
                     text_similarity(doc1, doc2),
-            # bag_of_words(s1, s2),
-            parse_similarity(doc1,doc2,s1,s2)
+            bag_of_words(s1, s2),
+            parse_similarity(s1,s2),
+            semanticSimilarity(s1,s2)
         ]
 
         # cosine similarity
@@ -345,11 +605,23 @@ def main(args):
 
     # clf = BaggingRegressor(SVR(kernel='linear'), n_estimators=15) # R1 uses default parameters as described in SVR documentation
     # clf = SVR(kernel='linear')
-    clf.fit(X_features, true_score)
+    # clf.fit(X_features, true_score)
     model2 = AdaBoostClassifier(n_estimators=50,
                                 learning_rate=1)
-    model2.fit(X_features, true_score)
-    # Testing
+    # model2.fit(X_features, true_score)
+    model3 = SVR(kernel='linear')
+    # model3.fit(X_features, true_score)
+    # from sklearn.externals import joblib
+
+    # Save the model as a pickle in a file
+    # joblib.dump(model3, 'model3.pkl')
+
+
+    eclf = VotingClassifier(estimators=[('Random Forest', clf), ('Adaboost', model2), ('SVR', model3)],
+                            voting ='soft')
+    eclf.fit(X_features, true_score)
+
+# Testing
     first_sents = []
     second_sents = []
     ids = []
@@ -379,23 +651,24 @@ def main(args):
         scores = [
                   sentence_similarity_word_alignment(s1,s2),
                   text_similarity(nlp(s1),nlp(s2)),
-            # bag_of_words(s1, s2),
-            parse_similarity(doc1,doc2,s1,s2)
-
+            bag_of_words(s1, s2),
+            parse_similarity(s1,s2),
+            semanticSimilarity(s1,s2)
             ]
         feature_scores.append(scores)
 
     X_features = scaler.transform(feature_scores)
-    Y_pred_np = clf.predict(X_features)
-    Y_model2 = model2.predict(X_features)
+    # Y_pred_np = clf.predict(X_features)
+    # Y_model2 = model2.predict(X_features)
+    # Y_model3 = model3.predict(X_features)
+
+    Y_eclf = eclf.predict(X_features)
+
     # Y_pred_np = [min(5,max(0,p),p) for p in int(Y_pred_np)]
     with open(args.predfile,'w') as f_pred:
         f_pred.write('Input_Id \t Predicted_Tage\n')
-        for i in range(len(Y_model2)):
-            pred = []
-            pred.append(int(Y_pred_np[i]))
-            pred.append(int(Y_model2[i]))
-            f_pred.write(ids[i]+"\t"+str(Average(pred))+'\n')
+        for i in range(len(Y_eclf)):
+            f_pred.write(ids[i]+"\t"+str(Y_eclf[i])+'\n')
 
 def text_similarity(inp_obj1, inp_obj2):
     return inp_obj1.similarity(inp_obj2)
@@ -437,7 +710,7 @@ def Average(lst):
         average = sum / len(lst)
     return round(average)
 
-def parse_similarity(doc1, doc2,s1,s2):
+def parse_similarity(s1,s2):
     s1 = s1.translate(str.maketrans('', '', string.punctuation))
     s2 = s2.translate(str.maketrans('', '', string.punctuation))
     # s2 = s2.replace(".","")
